8e23789978dfa25f879468777680f161f3a73c0c
Gloeend <alexey.larionov.fl@gmail.com>
2024-06-21 03:59:45 +0700

    upd

--- src/shared/components/StoreHeader/StoreHeader.tsx
+++ src/shared/components/StoreHeader/StoreHeader.tsx
@@ -9,7 +9,7 @@ type StoreHeaderProps = {
   tab: number
 }
 export default function StoreHeader(props: StoreHeaderProps) {
-  const { types, tab } = props;
+  const {types, tab} = props;
   const t = useTranslations('Index.Header');
   const locale = useLocale();
   return (
@@ -22,6 +22,11 @@ export default function StoreHeader(props: StoreHeaderProps) {
             <p>{t("description")}</p>
             <Link href={`${locale}/servers`}>
               <div className="dopInfoHeaderShopBox">
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" clipRule="evenodd"
+                        d="M0 10C0 4.47715 4.47715 0 10 0C15.5228 0 20 4.47715 20 10C20 15.5228 15.5228 20 10 20C4.47715 20 0 15.5228 0 10ZM10 5C10.5523 5 11 5.44772 11 6V10C11 10.5523 10.5523 11 10 11C9.44771 11 9 10.5523 9 10V6C9 5.44772 9.44771 5 10 5ZM9 14C9 13.4477 9.44772 13 10 13C10.5523 13 11 13.4477 11 14C11 14.5523 10.5523 15 10 15C9.44772 15 9 14.5523 9 14Z"
+                        fill="#8774B8"/>
+                </svg>
                 <p>{t("link")}</p>
               </div>
             </Link>
